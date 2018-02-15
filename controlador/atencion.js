3c4c0132377eb5d5a52892550b84777827ae1443
jsotteccanic <jsotteccanic@gmail.com>
2018-02-14 23:21:03 -0500

    atencion de ticket (funcional)

--- controlador/atencion.js
+++ controlador/atencion.js
@@ -9,20 +9,22 @@ function registrarAtencion(req, res) {
     var ticket = new Atencion();
     ticket.tipoAtencion = data.tipoAtencion;
     ticket.fecha_inicio = new Date(data.fecha_inicio);
-    ticket.fecha_fin = new Date(data.fecha_fin);
+    if(data.fecha_fin){
+        ticket.fecha_fin = new Date(data.fecha_fin);
+    }    
     ticket.descripcion = data.descripcion;
     ticket.colaborador = data.colaborador;
     ticket.usuario = data.usuario;
 
-    ticket.save(function (err, res) {
+    ticket.save(function (err, regTicket) {
         if (err) {
             res.status(500).send({ mensaje: 'Error al crear la atención' });
         } else {
-            console.log(res);
-            if (!res) {
+            console.log(regTicket);
+            if (!regTicket) {
                 res.status(404).send({ mensaje: 'No se ha registrado la atención' });
             } else {
-                res.status(200).send({ atencion: res });
+                res.status(200).send({ atencion: regTicket });
             }
         }
     })
